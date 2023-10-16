de5e60fcea0510addf7580f5885e6a91100b8173
Ricy <1932353948@qq.com>
2023-10-16 17:55:54 +0800

    feat: mobile navigation bar

--- src/modules/NavBar/Mobile.tsx
+++ src/modules/NavBar/Mobile.tsx
@@ -10,14 +10,14 @@ const NavLink: React.FC<ComponentProps<typeof Link> & { curPath: string }> = ({
 }) => (
   <li
     className={cx(
-      "relative pl-40px flex items-center w-full h-48px dropdown-shadow",
+      "relative pl-[40px] flex items-center w-full h-[48px] dropdown-shadow",
       {
         ["nav-link-mobile--active"]: curPath?.startsWith(href as string),
       }
     )}
   >
     <Link
-      className="flex items-center w-full h-full decoration-none"
+      className="flex items-center w-full h-full decoration-none "
       href={href}
     >
       {children}
@@ -32,16 +32,19 @@ const Mobile: React.FC<{ open: boolean; curPath: string }> = ({
   return (
     <div
       className={cx(
-        "nav-mobile sm:display-none absolute w-full h-[calc(100vh-80px)] left-0 top-80px bg-#F7F7F7 transition-transform duration-300 z-50 translate-y-[-100vh]",
-        open && "translate-y-0px"
+        "nav-mobile sm:display-none absolute w-full h-[calc(100vh-80px)] left-[0] top-[80px] bg-[#FF6B6B] transition-transform duration-300 z-[50] -translate-y-[100vh]",
+        open && "translate-y-[0px]"
       )}
     >
-      <ul className="pl-0px m-0px flex flex-col gap-12px text-22px font-semibold">
+      <ul className="pl-[0px] m-[0px] flex flex-col gap-[12px] text-[22px] font-semibold text-[#ffffff]">
         <NavLink href="/" curPath={curPath}>
           home
         </NavLink>
-        <NavLink href="/Create" curPath={curPath}>
+        <NavLink href="/create" curPath={curPath}>
           Create
+        </NavLink>
+        <NavLink href="/create" curPath={curPath}>
+          view
         </NavLink>
       </ul>
     </div>
